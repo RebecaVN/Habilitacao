54a11a5a447504ae1c99b2a7e221ff28f2ccebc8
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-12 09:35:17 -0300

    Add files via upload

---
+++ Habilitação.java
@@ -0,0 +1,32 @@
+
+public class Habilitação {
+	
+	public static void main (String [] args){
+		
+	String nomeDaPessoa1 = "Antônio";
+	String nomeDaPessoa2 = "Luiz";
+	
+	int idadeDaPessoa1 = 16;
+	int idadeDaPessoa2 = 18;
+	
+	
+	
+	System.out.println();
+	
+	if(idadeDaPessoa1 >=18) {
+	System.out.println(nomeDaPessoa1 + " Você pode tirar a habilitação.");
+
+	}else{
+		System.out.println(nomeDaPessoa1 + " você não pode tirar a habilitação.");
+	}
+	
+	if (idadeDaPessoa2 >=18) {
+		System.out.println(nomeDaPessoa2 + " você pode tirar a habilitação.");
+		
+	}else{
+		System.out.println(nomeDaPessoa2 + " você não pode tirar a habilitação.");
+	}
+
+		
+	}
+}
